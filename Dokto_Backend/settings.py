f45e27965970dc06c71606f527b87d3c143542d4
Sihan Tawsik <sihantawsik@gmail.com>
2021-09-28 20:04:32 +0600

    Update settings.py configuration
    Update settings configuration and isolate secret keys

--- Dokto_Backend/settings.py
+++ Dokto_Backend/settings.py
@@ -11,21 +11,35 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Read .env file
+dotenv_file = BASE_DIR / '.env'
+if dotenv_file.exists():
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^4b)*2-f3(wzxg&a1x!^+^$$(r!#ae)1x_4(!_#!radvf4v)t'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+debug_value = os.getenv("DEBUG", "True")
+if debug_value == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+__version__ = os.getenv("VERSION", "v1")
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -41,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +138,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Extra settings
+USE_X_FORWARDED_HOST = True
+SITE_ID = 1
