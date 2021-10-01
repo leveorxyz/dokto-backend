4e94c4d045c5cd99658917ce432026a0f905de0a
Sihan Tawsik <sihantawsik@gmail.com>
2021-10-01 20:47:52 +0600

    Update core/exceptions.py

--- core/exceptions.py
+++ core/exceptions.py
@@ -6,6 +6,19 @@ from .literals import INIT_CART
 
 def custom_exception_handler(exception, context):
     response = exception_handler(exception, context)
+    if response:
+        data = response.data
+        message_list = []
+        for _, v in data.items():
+            if isinstance(v, str):
+                message_list.append(v)
+            else:
+                message_list.append(" ".join([str(exc) for exc in v]))
+        custom_response = {
+            "message": " ".join(message_list),
+            "status_code": response.status_code,
+        }
+        response.data = custom_response
     return response
 
 
