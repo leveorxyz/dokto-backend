b989782b0393237353b0b770e1dec08665a450e7
Mahmudul Alam <expelmahmud@gmail.com>
2021-10-03 21:41:18 +0600

    add: comment

--- accounting/serializers.py
+++ accounting/serializers.py
@@ -3,5 +3,8 @@ from rest_framework import serializers
 
 
 class StripeChargeSerializer(serializers.Serializer):
+    """
+    Stripe Charge Serializer
+    """
     order_id = serializers.IntegerField()
     token = serializers.CharField()
