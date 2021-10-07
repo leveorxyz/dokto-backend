1d4f0f3974dc43f047aa444e550ff605a257f078
Sihan <sihantawsik@gmail.com>
2021-10-08 00:22:41 +0600

    Add flutterwave verification view

--- accounting/urls.py
+++ accounting/urls.py
@@ -3,15 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("stripe-charge/", views.StripeChargeAPIView.as_view(), name="stripe_charge"),
     path(
-        "paypal-verify/",
-        views.PaypalProcessWebhookAPIView.as_view(),
-        name="paypal_verify",
-    ),
-    path(
-        "paystack-verify/",
-        views.PaystackProcessWebhookAPIView.as_view(),
-        name="paystack_verify",
+        "flutterwave-verify/",
+        views.FlutterwaveVerifyAPIView.as_view(),
+        name="flutterwave_verify",
     ),
 ]
