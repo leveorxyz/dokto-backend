92755399c1b762b2212c75aedd8480a4dba57f61
Sihan <sihantawsik@gmail.com>
2021-10-07 21:09:49 +0600

    Add update custom error response

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
+from rest_framework.exceptions import ErrorDetail
 
 
 def custom_exception_handler(exception, context):
@@ -7,15 +8,22 @@ def custom_exception_handler(exception, context):
     if response:
         data = response.data
         message_list = []
-        for k, v in data.items():
-            print(k)
-            if isinstance(v, str):
-                message_list.append(v)
-            else:
-                message_list.append(" ".join([str(exc) for exc in v]))
+        if isinstance(data, dict):
+            for k, v in data.items():
+                if isinstance(v, str):
+                    message_list.append(v)
+                else:
+                    message_list.append(" ".join([str(exc) for exc in v]))
+        elif isinstance(data, list):
+            for item in data:
+                if isinstance(item, str):
+                    message_list.append(item)
+                elif isinstance(item, ErrorDetail):
+                    message_list.append(item.detail)
         custom_response = {
             "message": " ".join(message_list),
             "status_code": response.status_code,
+            "result": None
         }
         response.data = custom_response
     return response
