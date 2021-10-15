ae8a49c3f50d9dd9bbd173369af9b08c9453ee7f
Sihan <sihantawsik@gmail.com>
2021-10-15 22:55:41 +0600

    Update logout method

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.status import HTTP_404_NOT_FOUND
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, logout
 
 from core.views import CustomRetrieveAPIView
 from core.utils import set_user_ip
@@ -103,11 +103,9 @@ class LogoutView(APIView):
         - 401: Invalid credentials
         """
 
-        # Extracting user and token from request
-        user = request.user
-        token = request.auth
-        Token.objects.filter(user=user, key=token).delete()
+        # Flushing current request session
         set_user_ip(request)
+        logout(request)
         return Response(
             {
                 "status_code": 200,
