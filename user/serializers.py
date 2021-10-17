34815ade8511bb66a5ae7a4ac5f607bb8d297d50
Sihan <sihantawsik@gmail.com>
2021-10-17 20:57:14 +0600

    Add profile_photo field for doctor registration

--- user/serializers.py
+++ user/serializers.py
@@ -1,3 +1,5 @@
+import os
+
 from rest_framework.serializers import (
     ModelSerializer,
     Serializer,
@@ -5,8 +7,10 @@ from rest_framework.serializers import (
     SerializerMethodField,
 )
 from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 from core.literals import PROFILE_PHOTO_DIRECTORY
+from core.serializers import ReadWriteSerializerMethodField
 from .models import User, DoctorInfo
 from .utils import create_user, generate_image_file
 
@@ -31,10 +35,17 @@ class DoctorSerializer(ModelSerializer):
     city = CharField(write_only=True)
     zip_code = CharField(write_only=True)
     contact_no = CharField(write_only=True)
+    profile_photo = ReadWriteSerializerMethodField()
 
     def get_token(self, user: User):
         token, _ = Token.objects.get_or_create(user=user)
         return token.key
+
+    def get_profile_photo(self, user: User):
+        profile_photo = DoctorInfo.objects.filter(user=user).values_list(
+            "profile_photo", flat=True
+        )[0]
+        return os.path.join(settings.MEDIA_URL, PROFILE_PHOTO_DIRECTORY, profile_photo)
 
     def create(self, validated_data):
         user = create_user(validated_data)
@@ -66,4 +77,5 @@ class DoctorSerializer(ModelSerializer):
             "city",
             "zip_code",
             "contact_no",
+            "profile_photo",
         ]
