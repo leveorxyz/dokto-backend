3fc2d70de4c915ebfd55389499b25f3813e63d6f
Promit Basak <basakpromit@gmail.com>
2021-10-30 23:37:37 +0600

    Update operation field parameter for education serializer for consistent response

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -194,7 +194,7 @@ class DoctorEducationUpdateSerializerWithID(ModelSerializer):
     `dashboard > profile settings > experience and education`.
     """
 
-    operation = CharField(required=False, allow_null=True)
+    operation = CharField(required=False, allow_null=True, write_only=True)
     certificate = ReadWriteSerializerMethodField(required=False, allow_null=True)
 
     def get_certificate(self, doctor_education: DoctorEducation):
