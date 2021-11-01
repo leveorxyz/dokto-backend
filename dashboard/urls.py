a4006337101628c93c3065cbb95d9f2a3b581a40
Promit Basak <basakpromit@gmail.com>
2021-11-01 14:08:40 +0600

    Add trailing slash for urlpattherns and fix ambiguous urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,25 +10,25 @@ from .views import (
 
 urlpatterns = [
     path(
-        "doctor/<str:username>", DoctorProfileAPIView.as_view(), name="doctor-profile"
+        "doctor/<str:username>/", DoctorProfileAPIView.as_view(), name="doctor-profile"
     ),
     path(
-        "doctor/profile-settings/profile-detail/<str:username>",
+        "doctor/profile-settings/profile-detail/<str:username>/",
         DoctorProfileDetailsAPIView.as_view(),
         name="doctor-profile-detail",
     ),
     path(
-        "doctor/profile-settings/education-experience/<str:username>",
+        "doctor/profile-settings/education-experience/<str:username>/",
         DoctorEducationExperienceSettingsAPIView.as_view(),
         name="doctor-education-experience",
     ),
     path(
-        "doctor/profile-settings/available-hours/<str:username>",
+        "doctor/profile-settings/available-hours/<str:username>/",
         DoctorAvailableHoursSettingsAPIView.as_view(),
         name="doctor-available-hours",
     ),
     path(
-        "doctor/specialty-settings/<str:username>",
+        "doctor/specialty-settings/<str:username>/",
         DoctorSpecialtySettingsAPIView.as_view(),
         name="doctor-specialty-settings",
     ),
