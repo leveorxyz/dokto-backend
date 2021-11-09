a652dcc3bd555c6b022ba9b3be44cf0c93a4f054
Sihan <sihantawsik@gmail.com>
2021-11-09 22:18:04 +0600

    Add appointment video chat token api

--- twilio_chat/urls.py
+++ twilio_chat/urls.py
@@ -6,5 +6,10 @@ from . import views
 urlpatterns = [
     path(
         "video-token/", views.VideoChatTokenCreateAPIView.as_view(), name="video_token"
-    )
+    ),
+    path(
+        "appointment-video-token/",
+        views.AppointmentVideoChatTokenCreateAPIView.as_view(),
+        name="appointment_video_token",
+    ),
 ]
--- twilio_chat/views.py
+++ twilio_chat/views.py
@@ -7,7 +7,7 @@ from twilio.jwt.access_token.grants import VideoGrant
 from django.conf import settings
 
 
-from .serializers import VideoChatTokenSerializer
+from .serializers import VideoChatTokenSerializer, AppointmentVideoChatTokenSerializer
 
 
 class VideoChatTokenCreateAPIView(generics.CreateAPIView):
@@ -38,7 +38,42 @@ class VideoChatTokenCreateAPIView(generics.CreateAPIView):
             "result": {
                 "token": token.to_jwt(),
                 "identity": token.identity,
-                "room": room
+                "room": room,
+            },
+        }
+        return Response(data=data, status=status.HTTP_201_CREATED)
+
+
+class AppointmentVideoChatTokenCreateAPIView(generics.CreateAPIView):
+    """
+    View for handling twilio video chat access token generation.
+    """
+
+    serializer_class = AppointmentVideoChatTokenSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.data
+
+        token = AccessToken(
+            settings.TWILIO_ACCOUNT_SID,
+            settings.TWILIO_API_KEY,
+            settings.TWILIO_API_SECRET,
+            identity=validated_data.get("id"),
+        )
+        rooms = validated_data.get("room_name")
+        for room in rooms:
+            token.add_grant(VideoGrant(room=room))
+
+        data = {
+            "status_code": 201,
+            "message": "Success",
+            "result": {
+                "token": token.to_jwt(),
+                "identity": token.identity,
+                "room": rooms,
             },
         }
         return Response(data=data, status=status.HTTP_201_CREATED)
