1baed038fea5747b1a1590d9b48a972865b95a60
Sihan <sihantawsik@gmail.com>
2021-11-11 00:37:19 +0600

    Fix twilio appointment token generation

--- twilio_chat/views.py
+++ twilio_chat/views.py
@@ -61,7 +61,7 @@ class AppointmentVideoChatTokenCreateAPIView(generics.CreateAPIView):
             settings.TWILIO_ACCOUNT_SID,
             settings.TWILIO_API_KEY,
             settings.TWILIO_API_SECRET,
-            identity=validated_data.get("id"),
+            identity=validated_data.get("identity"),
         )
 
         token.add_grant(VideoGrant())
