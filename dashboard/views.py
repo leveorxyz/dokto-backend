a17cea2b5cc425ebd71bc23f0eddf7120aa71e27
Promit Basak <basakpromit@gmail.com>
2021-11-24 20:42:54 +0600

    Update dashboard API urls (exclude username)

--- dashboard/views.py
+++ dashboard/views.py
@@ -55,9 +55,7 @@ class DoctorProfileDetailsAPIView(CustomRetrieveUpdateAPIView):
         return DoctorInfo.objects.all()
 
     def get_object(self):
-        obj = get_object_or_404(
-            self.get_queryset(), username=self.kwargs.get("username")
-        )
+        obj = get_object_or_404(self.get_queryset(), user=self.request.user)
         self.check_object_permissions(self.request, obj)
         return obj
 
@@ -76,9 +74,7 @@ class DoctorEducationExperienceSettingsAPIView(CustomRetrieveUpdateAPIView):
         return DoctorInfo.objects.all()
 
     def get_object(self) -> DoctorInfo:
-        obj = get_object_or_404(
-            self.get_queryset(), username=self.kwargs.get("username")
-        )
+        obj = get_object_or_404(self.get_queryset(), user=self.request.user)
         self.check_object_permissions(self.request, obj)
         return obj
 
@@ -93,9 +89,7 @@ class DoctorAvailableHoursSettingsAPIView(CustomListUpdateAPIView):
             return DoctorAvailableHoursSerializerWithID
 
     def get_queryset(self):
-        doctor_info = get_object_or_404(
-            DoctorInfo, username=self.kwargs.get("username")
-        )
+        doctor_info = get_object_or_404(DoctorInfo, user=self.request.user)
         self.check_object_permissions(self.request, doctor_info)
         return doctor_info.doctoravailablehours_set.all()
 
@@ -114,9 +108,7 @@ class DoctorSpecialtySettingsAPIView(CustomRetrieveUpdateAPIView):
         return DoctorInfo.objects.all()
 
     def get_object(self):
-        obj = get_object_or_404(
-            self.get_queryset(), username=self.kwargs.get("username")
-        )
+        obj = get_object_or_404(self.get_queryset(), user=self.request.user)
         self.check_object_permissions(self.request, obj)
         return obj
 
