b18781218b1a802e4e06c62bf5bc089a9eb52bd7
Sihan <sihantawsik@gmail.com>
2021-11-24 14:54:50 +0600

    Add encountered patient list

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+
+from core.views import CustomListAPIView, CustomListCreateAPIView
+from core.permissions import DoctorPermission
+from .models import Appointment
+from .serializers import AppointmentSerializer
 
 # Create your views here.
+class AppointmentListCreateAPIView(CustomListCreateAPIView):
+    """
+    Appointment list create API view
+    """
+
+    permission_classes = (IsAuthenticated,)
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+
+class EncounteredPatientListAPIView(CustomListAPIView):
+    """
+    List of encountered patients
+    """
+
+    permission_classes = [IsAuthenticated, DoctorPermission]
