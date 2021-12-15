0ec449429bf9d55fd0c257b92289f2f3c054ee59
Sihan <sihantawsik@gmail.com>
2021-12-15 20:45:16 +0600

    add: Add schema view on onboardapi

--- onboarding/views.py
+++ onboarding/views.py
@@ -1,16 +1,22 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import (
+    extend_schema,
+)
+from drf_spectacular.types import OpenApiTypes
 
 from core.views import CustomAPIView
 from core.permissions import ClinicPermission
-
-# Create your views here.
+from .serializers import OnboardSerializer
 
 
 class OnboardMailAPIView(CustomAPIView):
     http_method_names = ["post", "options"]
 
+    @extend_schema(
+        request=OnboardSerializer,
+    )
     @permission_classes((IsAuthenticated, ClinicPermission))
     def post(self, request, *args, **kwargs):
         if "doctor_id" not in request.data:
