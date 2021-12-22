839c5e777d0d72af7d796d2a0a7bb42d68fa7c0a
Sihan <sihantawsik@gmail.com>
2021-12-22 13:16:47 +0600

    add: Inbox list view

--- inbox/serializers.py
+++ inbox/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework.serializers import ModelSerializer
+
+from .models import InboxChannel
+
+
+class InboxChannelSerializer(ModelSerializer):
+    class Meta:
+        model = InboxChannel
+        fields = "__all__"
--- inbox/views.py
+++ inbox/views.py
@@ -1,7 +1,15 @@
+from django.db.models import Q
+
 from core.views import CustomListAPIView
+from .models import InboxChannel
+from .serializers import InboxChannelSerializer
 
 # Create your views here.
 
 
 class InboxChannelListView(CustomListAPIView):
-    ...
+    serializer_class = InboxChannelSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return InboxChannel.objects.filter(Q(first_user=user) | Q(second_user=user))
