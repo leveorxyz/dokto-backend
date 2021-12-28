f714607c3530f31c7e8006fe69e4d8be54a4d46a
Sihan <sihantawsik@gmail.com>
2021-12-29 01:59:01 +0600

    add: License info to PharmacyInfo

--- user/models.py
+++ user/models.py
@@ -18,6 +18,7 @@ from core.literals import (
     DOCTOR_LICENSE_FILE_DIRECTORY,
     PATIENT_IDENTIFICATION_PHOTO_DIRECTORY,
     CLINIC_LICENSE_FILE_DIRECTORY,
+    PHARMACY_LICENSE_FILE_DIRECTORY,
 )
 from core.modelutils import send_mail
 from .utils import generate_file_and_name
@@ -504,6 +505,33 @@ class PharmacyInfo(CoreModel):
     notification_email = models.EmailField(blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     website = models.URLField(blank=True, null=True, default=None)
+    _license_file = models.FileField(
+        upload_to=PHARMACY_LICENSE_FILE_DIRECTORY, blank=True, null=True, default=None
+    )
+    license_expiration = models.DateField(blank=True, null=True)
+
+    @property
+    def license_file(self):
+        domain = Site.objects.get_current().domain
+        if self._license_file.name:
+            return domain + self._license_file.url
+
+    @license_file.setter
+    def license_file(self, license_file_data):
+        if self._license_file.name:
+            del self.license_file
+        file_name, file = generate_file_and_name(license_file_data, self.id)
+        self._license_file.save(file_name, file, save=True)
+        self.save()
+
+    @license_file.deleter
+    def license_file(self):
+        if self._license_file.name:
+            self._license_file.delete(save=True)
+
+    def delete(self, *args, **kwargs):
+        del self.license_file
+        return super(PharmacyInfo, self).delete(*args, **kwargs)
 
     @classmethod
     def get_hidden_fields(cls):
