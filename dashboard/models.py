20c78f428cb6db503f47b9c2c064e0a255df91e9
Sihan <sihantawsik@gmail.com>
2021-12-30 00:40:36 +0600

    add: Hospital team

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from core.models import CoreModel
+from user.models import ClinicInfo, DoctorInfo
+
 # Create your models here.
+
+
+class HospitalTeam(CoreModel):
+    clinic = models.ForeignKey(ClinicInfo, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(DoctorInfo, on_delete=models.CASCADE)
+    profession = models.CharField(max_length=100, null=True, blank=True)
